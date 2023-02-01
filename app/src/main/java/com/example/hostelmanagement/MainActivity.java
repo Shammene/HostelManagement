bc004248a709d60b7271812957849de717e36a4a
Shammene <shammene9905@gmail.com>
2023-02-01 14:51:16 +0800

    login build

--- app/src/main/java/com/example/hostelmanagement/MainActivity.java
+++ app/src/main/java/com/example/hostelmanagement/MainActivity.java
@@ -7,28 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button goToSignUpActivity= findViewById(R.id.goToSignUpActivity);
-        Button goToLoginActivity= findViewById(R.id.goToLoginActivity);
-
-        goToSignUpActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,SignupActivity.class) );
-            }
-        });
-
-        goToLoginActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,LoginActivity.class) );
-            }
-        });
     }
 }
