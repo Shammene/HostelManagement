e89675727dfbd4e26bd9613cc4d4a6be9c223b92
Shammene <shammene9905@gmail.com>
2023-02-07 02:33:24 +0800

    update sensor

--- app/src/main/java/com/example/hostelmanagement/Main1.java
+++ app/src/main/java/com/example/hostelmanagement/Main1.java
@@ -4,26 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
-public class Main1 extends AppCompatActivity{
+public class Main1 extends AppCompatActivity implements View.OnClickListener {
 
     BiometricPrompt biometricPrompt;
     BiometricPrompt.PromptInfo promptInfo;
-    ConstraintLayout mMainLayout;
+    LinearLayout mMainLayout;
+    Button login,signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
         mMainLayout=findViewById(R.id.main_Layout);
+        login=findViewById(R.id.login);
+        signup=findViewById(R.id.signup);
+
+        login.setOnClickListener(this);
 
         BiometricManager biometricManager=BiometricManager.from(this);
         switch (biometricManager.canAuthenticate()) {
@@ -46,7 +54,6 @@ public class Main1 extends AppCompatActivity{
             }
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-                Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
                 mMainLayout.setVisibility(View.VISIBLE);
             }
             public void onAuthenticationFailed() {
@@ -58,5 +65,19 @@ public class Main1 extends AppCompatActivity{
                 .setDescription("Use Fingerprint to login").setDeviceCredentialAllowed(true).build();
 
         biometricPrompt.authenticate(promptInfo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.login:
+                Toast.makeText(this,"login",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Main1.this, LoginActivity.class));
+                break;
+            case R.id.signup:
+                Toast.makeText(this,"signup",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Main1.this, SignupActivity.class));
+                break;
+        }
     }
 }
