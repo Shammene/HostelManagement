4700beb1e29f1dc223ebf260d48ab9db3325dd0c
Shammene <shammene9905@gmail.com>
2023-02-07 01:24:45 +0800

    sensor for authentication

---
+++ app/src/main/java/com/example/hostelmanagement/Main1.java
@@ -0,0 +1,63 @@
+package com.example.hostelmanagement;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+
+public class Main1 extends AppCompatActivity {
+
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    ConstraintLayout mMainLayout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+        mMainLayout=findViewById(R.id.main_Layout);
+
+        BiometricManager biometricManager=BiometricManager.from(this);
+        switch(biometricManager.canAuthenticate())
+        {
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(),"Device Doesnt have fingerprint",Toast.LENGTH_SHORT).show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(getApplicationContext(),"Not Working",Toast.LENGTH_SHORT).show();
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(getApplicationContext(),"No fingerprint Assigned",Toast.LENGTH_SHORT).show();
+        }
+        Executor executor= ContextCompat.getMainExecutor(this);
+
+        biometricPrompt=new BiometricPrompt(Main1.this,executor,new BiometricPrompt.AuthenticationCallback(){
+            public void onAuthenticationError(int errorcode, @NonNull CharSequence errString){
+                super.onAuthenticationError(errorcode,errString);
+            }
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+                mMainLayout.setVisibility(View.VISIBLE);
+            }
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        promptInfo=new BiometricPrompt.PromptInfo.Builder().setTitle("MAD1C")
+                .setDescription("Use Fingerprint to login").setDeviceCredentialAllowed(true).build();
+
+        biometricPrompt.authenticate(promptInfo);
+    }
+}
